390565f9e776debfc98169b34670eb2acef19fa5
prasad123-hub <narkhedkarprasad@gmail.com>
2023-04-30 14:22:22 +0530

    fix : description

--- config/site.ts
+++ config/site.ts
@@ -2,7 +2,7 @@ import { SiteConfig } from '@/types'
 
 export const siteConfig: SiteConfig = {
   name: 'DevUI',
-  description: 'An open source tailwind css component library.',
+  description: 'An tailwind css component library.',
   url: 'https://devui.io',
   ogImage: '',
 }
